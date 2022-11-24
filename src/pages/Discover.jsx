887479a6563d64f21c301720579136c859b918ca
4eug <debraheug@gmail.com>
2022-11-24 21:03:30 +0000

    Revamp of Discover page

--- src/pages/Discover.jsx
+++ src/pages/Discover.jsx
@@ -1,12 +1,14 @@
 /* eslint-disable jsx-a11y/control-has-associated-label */
-import { useDispatch, useSelector } from 'react-redux';
+import { useSelector } from 'react-redux';
+// eslint-disable-next-line import/no-cycle
 import { Error, Loader, SongCard } from '../components';
-import { genres } from '../assets/constants';
+// import { genres } from '../assets/constants';
 // eslint-disable-next-line import/named
 import { useGetTopChartsQuery } from '../redux/services/shazamCore';
+// import { selectGenreListId } from '../redux/features/playerSlice';
 
 const Discover = () => {
-  const dispatch = useDispatch();
+  // const dispatch = useDispatch();
   const { activeSong, isPlaying } = useSelector((state) => state.player);
   const { data, isFetching, error } = useGetTopChartsQuery();
   const genreTitle = 'Pop';
@@ -19,13 +21,13 @@ const Discover = () => {
     <div className="flex flex-col">
       <div className="w-full flex justify-between items-center sm:flex-row flex-col mt-4 mb-10">
         <h2 className="font-bold text-3xl text-white text-left">Discover {genreTitle}</h2>
-        <select
-          onChange={() => {}}
-          value=""
+        {/* <select
+          onChange={(e) => dispatch(selectGenreListId(e.target.value))}
+          value={genreListId || 'pop'}
           className="bg-black text-gray-300 p-3 text-sm rounded-lg outline-none sm:mt-0 mt-5"
         >
           {genres.map((genre) => <option>{genre.title}</option>)}
-        </select>
+        </select> */}
       </div>
       <div className="flex flex-wrap sm:justify-start justify-center gap-8">
         {data?.map((song, i) => (
--- src/redux/services/shazamCore.js
+++ src/redux/services/shazamCore.js
@@ -1,4 +1,5 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+import { genres } from '../../assets/constants';
 
 export const shazamCoreApi = createApi({
   reducerPath: 'shazamCoreApi',
@@ -12,13 +13,21 @@ export const shazamCoreApi = createApi({
   }),
   endpoints: (builder) => ({
     getTopCharts: builder.query({ query: () => '/charts/world' }),
+    getSongsByGenre: builder.query({ query: () => `/charts/genre-world?genre_code=${genres}` }),
     getSongDetails: builder.query({ query: ({ songid }) => `/tracks/details?track_id=${songid}` }),
     getSongRelated: builder.query({ query: ({ songid }) => `/tracks/related?track_id=${songid}` }),
+    getArtistDetails: builder.query({ query: ({ artistid }) => `/artists/details?artist_id=${artistid}` }),
+    getSongsByCounrty: builder.query({ query: (countryCode) => `/charts/country?countryCode=${countryCode}` }),
+    getSongsBySearch: builder.query({ query: (searchTerm) => `/search/multi?search_type=SONGS_ARTISTS&query=${searchTerm}` }),
   }),
 });
 
 export const {
   useGetTopChartsQuery,
+  useGetSongsByGenreQuery,
   useGetSongDetailsQuery,
   useGetSongRelatedQuery,
+  useGetArtistDetailsQuery,
+  useGetSongsByCounrtyQuery,
+  useGetSongsBySearchQuery,
 } = shazamCoreApi;
